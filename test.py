c71a73528640d81b325e19aa38a3f68e4fe34366
Ingo Scholtes <scholtes@ifi.uzh.ch>
2019-01-08 17:42:58 +0100

    working!

--- test.py
+++ test.py
@@ -11,83 +11,122 @@ git_repo = pydriller.GitRepository(repo_string)
 df_commits = pd.DataFrame()
 df_modifications = pd.DataFrame()
 
-#%%
-c = git_repo.get_commit('322655441586e728cd68255ed40223485334cacd')
-for c_m in c.modifications:
-    blame_modification(git_repo, c, c_m)    
 
 #%%
-def line_num_pre_to_post(added_lines, deleted_lines, pre_line_num, offset = 0):
-    post_line_num = pre_line_num
-    
-    print('--------------')
-    print('deleted lines up to line ' + str(pre_line_num))
-    deleted = 0
-    for x in deleted_lines:
-        if x < pre_line_num:
-            print(str(x) + ': ' + deleted_lines[x])
-            deleted += 1            
 
-    # how many added lines have been deleted as well?
-    added = 0
+def chunk_size(lines, k):
+    if k not in lines or (k>1 and k-1 in lines):
+        chunk = False
+        chunksize = 0
+    else:
+        chunk = True
+        chunksize = 1
 
-    print('--------------')
-    print('added lines')
-    print('--------------')
-    for x in added_lines:
-        if x < pre_line_num:
-            print(str(x) + ': ' + added_lines[x])
-            added += 1
-    print('--------------')
-    return pre_line_num + added + offset
+    while chunk:
+        if k+chunksize in lines:
+            chunksize += 1
+        else:
+            chunk = False
+    return chunksize
 
 
-def blame_modification(git_repo, commit, mod):
-    
-    path = mod.new_path
+def pre_to_post(git_repo, commit, mod):    
     
     print('=======================')
     print('File: ' + mod.filename)
     print('=======================')
 
     diff = git_repo.parse_diff(mod.diff)
-    print('diff = ' + mod.diff)    
+    print('diff = ' + mod.diff)
+    
+    deleted_lines = { x[0]:x[1] for x in git_repo.parse_diff(mod.diff)['deleted'] }
+    added_lines = { x[0]:x[1] for x in git_repo.parse_diff(mod.diff)['added'] } 
+    max_added = max(added_lines.keys())
+    max_deleted = max(deleted_lines.keys())
+
+    # create mapping between pre and post edit line numbers
+    left_to_right = {}
+    right = 1
+    left = 1
+
+    no_right_inc = 0
+    both_inc = 0
+    jump_right = 0
+
+    while left <= max(max_added, max_deleted):
+
+        # compute size of added and deleted chunks
+        chunk_added = chunk_size(added_lines, right)
+        chunk_deleted = chunk_size(deleted_lines, left)
+
+        # deleted chunk is larger than added chunk
+        if chunk_deleted > chunk_added:
+            no_right_inc = chunk_deleted - chunk_added
+            both_inc = chunk_added
+
+        # added chunk is larger than deleted chunk
+        elif chunk_added > chunk_deleted:
+            jump_right = chunk_added - chunk_deleted
+            both_inc = chunk_deleted
+
+        # increment left and right counter
+        if both_inc>0:
+            both_inc -= 1
+            left_to_right[left] = right
+            left +=1            
+            right += 1
+        elif no_right_inc>0:
+                no_right_inc -= 1
+                left_to_right[left] = False                
+                left += 1
+        else:
+            right += jump_right
+            left_to_right[left] = right
+            left +=1            
+            right += 1
+            jump_right = 0
+
+    return left_to_right
+
+
+def blame_modification(git_repo, commit, mod):
+    path = mod.new_path
     deleted_lines = { x[0]:x[1] for x in git_repo.parse_diff(mod.diff)['deleted'] }
     added_lines = { x[0]:x[1] for x in git_repo.parse_diff(mod.diff)['added'] }
-    offset = 0
     try:
         blame = git_repo.git.blame(commit.hash + '^', '--', path).split('\n')
         for num_line, line in deleted_lines.items():
             blame_fields = blame[num_line - 1].split(' ')
-
             buggy_commit_hash = blame_fields[0].replace('^', '')
-            buggy_commit = git_repo.get_commit(buggy_commit_hash)            
+            buggy_commit = git_repo.get_commit(buggy_commit_hash)
             print('Previous commit: \t' + buggy_commit_hash)
             print('Author: \t\t' + buggy_commit.author.email)
             print('Date: \t\t\t' + buggy_commit.author_date.strftime('%Y-%m-%d %H:%M:%S'))
+            print('blame\t\t\t= ', blame[num_line-1])
 
-            post_line_num = line_num_pre_to_post(added_lines, deleted_lines, num_line, offset)
+            left_to_right = pre_to_post(git_repo, commit, mod)
+            right_to_left = { v:k for k,v in left_to_right.items() if v != False }
+
             print('deleted line num\t= ', num_line)
             print('deleted line\t\t= ', line)
-            print('blame\t\t\t= ', blame[num_line-1])
-            print('added line num\t\t= ', post_line_num)
-            if post_line_num in added_lines:
-                print('replaced line\t\t= ', added_lines[post_line_num])
-            else:
-                offset -= 1
-                print('deleted line')
             
-            for buggy_mod in buggy_commit.modifications:
-                if buggy_mod.filename == mod.filename:                          
-                    prev_diff = git_repo.parse_diff(buggy_mod.diff)                
-                    # for x in prev_diff['added']:
-                    #    print(x)
+            print('added line num\t\t= ', left_to_right[num_line])
+            if left_to_right[num_line] in added_lines:
+                print('added line\t\t= ', added_lines[left_to_right[num_line]])
+            else:
+                print('removed line')
             print('-----------------------------')
 
     except GitCommandError:
         logger.debug("Could not found file %s in commit %s. Probably a double rename!", mod.filename,
                         commit.hash)
 
+
+
+#%%
+c = git_repo.get_commit('322655441586e728cd68255ed40223485334cacd')
+for c_m in c.modifications:
+    blame_modification(git_repo, c, c_m)
 
 #%%
 def process_commit(commit):

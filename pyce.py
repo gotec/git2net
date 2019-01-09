69d9c6578fa1aafcbe2ed548159ba16cf39ffc46
Ingo Scholtes <scholtes@ifi.uzh.ch>
2019-01-09 12:22:27 +0100

    finished command line tool

--- pyce.py
+++ pyce.py
@@ -1,21 +1,33 @@
-#%%
-import pathpy as pp
-import pydriller as pydriller
+#!/usr/bin/python3
+
+import logging
 import sqlite3
+import os
+import argparse
+
+from multiprocessing import Pool
+from multiprocessing import Semaphore
+
 import pandas as pd
+import progressbar
+
+import pydriller as pydriller
 from pydriller.git_repository import GitCommandError
 from Levenshtein import distance as lev_dist
-import logging
+
 logger = logging.getLogger(__name__)
 
-repo_string = '~/pathpy/'
-git_repo = pydriller.GitRepository(repo_string)
 
-df_commits = pd.DataFrame()
-df_coedits = pd.DataFrame()
-
-#%%
 def chunk_size(lines, k):
+    """
+    Calculates the size of a chunk of added/deleted lines starting
+    in a given line k.
+
+    Parameters
+    ----------
+    @param lines: dictionary of added or deleted lines
+    @param k: line number to check for
+    """
     if k not in lines or (k>1 and k-1 in lines):
         chunk = False
         chunksize = 0
@@ -30,9 +42,11 @@ def chunk_size(lines, k):
             chunk = False
     return chunksize
 
-
-def pre_to_post(git_repo, commit, mod):    
-    
+def pre_to_post(git_repo, commit, mod):
+    """
+    Maps line numbers between the pre- and post-commit version
+    of a modification.
+    """
     #print('=======================')
     #print('File: ' + mod.filename)
     #print('=======================')
@@ -97,7 +111,8 @@ def pre_to_post(git_repo, commit, mod):
 
 
 def extract_coedits(git_repo, commit, mod):
-    global df_coedits
+    
+    df = pd.DataFrame()
 
     path = mod.new_path
     deleted_lines = { x[0]:x[1] for x in git_repo.parse_diff(mod.diff)['deleted'] }
@@ -108,7 +123,7 @@ def extract_coedits(git_repo, commit, mod):
         for num_line, line in deleted_lines.items():
             blame_fields = blame[num_line - 1].split(' ')
             buggy_commit_hash = blame_fields[0].replace('^', '')
-            buggy_commit = git_repo.get_commit(buggy_commit_hash)
+            #buggy_commit = git_repo.get_commit(buggy_commit_hash)
             #print('Previous commit: \t' + buggy_commit_hash)
             #print('Author: \t\t' + buggy_commit.author.email)
             #print('Date: \t\t\t' + buggy_commit.author_date.strftime('%Y-%m-%d %H:%M:%S'))
@@ -116,10 +131,16 @@ def extract_coedits(git_repo, commit, mod):
 
             c = {}
             c['pre_commit'] = [buggy_commit_hash]
-            c['post_commit'] = [commit.hash]
-            c['path'] = [path]
+            c['post_commit'] = [commit.hash]            
             c['pre_line_len'] = [len(line)]
             c['pre_line_num'] = [num_line]
+            c['mod_filename'] = [mod.filename]
+            c['mod_cyclomatic_complexity'] = [mod.complexity()]
+            c['mod_loc'] = [mod.nloc()]
+            c['mod_new_path'] = [path]
+            c['mod_token_count'] = [mod.token_count()]
+            c['mod_removed'] = [mod.removed()]
+            c['mod_added'] = [mod.added()]
             
 
             left_to_right = pre_to_post(git_repo, commit, mod)
@@ -141,24 +162,18 @@ def extract_coedits(git_repo, commit, mod):
                 c['post_line_num'] = [None]
                 c['levenshtein_dist'] = [None]
             df_t = pd.DataFrame(c)
-            df_coedits = pd.concat([df_coedits, df_t])
-            # print('-----------------------------')
+            df = pd.concat([df, df_t])
 
     except GitCommandError:
         logger.debug("Could not found file %s in commit %s. Probably a double rename!", mod.filename,
                         commit.hash)
+    
+    return df
 
 
-
-#%%
-# c = git_repo.get_commit('322655441586e728cd68255ed40223485334cacd')
-# for c_m in c.modifications:
-#    extract_coedits(git_repo, c, c_m)
-
-
-#%%
 def process_commit(git_repo, commit):
-    global df_commits
+    df_commit = pd.DataFrame()
+    df_coedits = pd.DataFrame()
 
     # parse commit
     c = {}
@@ -169,23 +184,86 @@ def process_commit(git_repo, commit):
     c['committer_name'] = [commit.committer.name]
     c['author_date'] = [commit.author_date.strftime('%Y-%m-%d %H:%M:%S')]
     c['committer_date'] = [commit.committer_date.strftime('%Y-%m-%d %H:%M:%S')]
-    df_t = pd.DataFrame(c)
-    df_commits = pd.concat([df_commits, df_t])
+    c['committer_timezone'] = [commit.committer_timezone]
+    c['msg_len'] = [len(commit.msg)]
+    c['project_name'] = [commit.project_name]
+    c['parents'] = [ ','.join(commit.parents)]
+    c['merge'] = [commit.merge]
+    c['in_main_branch'] = [commit.in_main_branch]
+    c['branches'] = [ ','.join(commit.branches)]
+    
+    df_commit = pd.DataFrame(c)
     
     # parse modification
     for modification in commit.modifications:
-        extract_coedits(git_repo, commit, modification)
+        df = extract_coedits(git_repo, commit, modification)
+        df_coedits = pd.concat([df_coedits, df])
+    return df_commit, df_coedits
 
 
 
-#%%
-num_commits = 0
-for commit in pydriller.RepositoryMining(repo_string).traverse_commits():
-    print(num_commits)
-    num_commits += 1
-    process_commit(git_repo, commit)
+def process_repo_serial(repo_string):
+    
+    git_repo = pydriller.GitRepository(repo_string)
+    
+    df_commits = pd.DataFrame()
+    df_coedits = pd.DataFrame()
 
-#%%
-print(df_commits)
-print(df_coedits)
+    i = 0
+    commits = [c for c in git_repo.get_list_commits()]
+    with progressbar.ProgressBar(max_value=len(commits)) as bar:
+        for commit in commits:
+            df_1, df_2 = process_commit(git_repo, commit)
+            df_commits = pd.concat([df_commits, df_1])
+            df_coedits = pd.concat([df_coedits, df_2])
+            i += 1
+            bar.update(i)
+    return df_commits, df_coedits
 
+semaphore = Semaphore(1)
+
+
+def process_commit_parallel(arg):
+    repo_string, commit_hash, sqlite_db_file = arg
+    git_repo = pydriller.GitRepository(repo_string)
+
+    df_1, df_2 = process_commit(git_repo, git_repo.get_commit(commit_hash))    
+    
+    with semaphore:
+        con = sqlite3.connect(sqlite_db_file)
+        if not df_1.empty:
+            df_1.to_sql('commits', con, if_exists='append')
+        if not df_2.empty:
+            df_2.to_sql('coedits', con, if_exists='append')
+    return True
+
+def process_repo_parallel(repo_string, sqlite_db_file, num_processes=os.cpu_count()):
+    
+    print('Using {0} workers.'.format(num_processes))    
+    git_repo = pydriller.GitRepository(repo_string)
+    args = [ (repo_string, c.hash, sqlite_db_file) for c in git_repo.get_list_commits()]
+
+    p = Pool(num_processes)
+    with progressbar.ProgressBar(max_value=len(args)) as bar:
+        for i,_ in enumerate(p.imap(process_commit_parallel, args)):
+            bar.update(i)
+
+
+parser = argparse.ArgumentParser(description='Extracts commit and co-editing data from git repositories.')
+parser.add_argument('repo', help='path to repository to be parsed.', type=str)
+parser.add_argument('outfile', help='path to SQLite DB file storing results.', type=str)
+parser.add_argument('--parallel', help='use multi-core processing. Default.', dest='parallel', action='store_true')
+parser.add_argument('--no-parallel', help='do not use multi-core processing.', dest='parallel', action='store_false')
+parser.add_argument('--numprocesses', help='number of CPU cores to use for multi-core processing. Defaults to number of CPU cores.', default=os.cpu_count(), type=int)
+parser.set_defaults(parallel=True)
+
+args = parser.parse_args()
+if args.parallel:
+    process_repo_parallel(repo_string=args.repo, sqlite_db_file=args.outfile, num_processes=args.numprocesses)
+else:
+    df_commits, df_coedits = process_repo_serial(args.repo)
+    con = sqlite3.connect(args.outfile)
+    if not df_commits.empty:
+        df_commits.to_sql('commits', con, if_exists='append')
+    if not df_coedits.empty:
+        df_coedits.to_sql('coedits', con, if_exists='append')

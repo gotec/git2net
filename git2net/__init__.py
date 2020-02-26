b286a01cbc9e8a0502a29eb7d51478be0455618e
Christoph Gote <cgote@ethz.ch>
2020-02-26 19:17:08 +0100

    increased version number

--- git2net/__init__.py
+++ git2net/__init__.py
@@ -5,7 +5,7 @@ and time-stamped co-editing networks from git repositories.
 
 __author__ = "Christoph Gote"
 __email__ = "cgote@ethz.ch"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from .extraction import mine_git_repo
 from .extraction import get_unified_changes

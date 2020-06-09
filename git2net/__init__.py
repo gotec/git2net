8cba054cb40d805f2d164f8e719df02063a4f642
Christoph Gote <gotec@me.com>
2020-06-09 14:08:06 +0000

    new pypi release

--- git2net/__init__.py
+++ git2net/__init__.py
@@ -5,7 +5,7 @@ and time-stamped co-editing networks from git repositories.
 
 __author__ = "Christoph Gote"
 __email__ = "cgote@ethz.ch"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 from .extraction import mine_git_repo
 from .extraction import get_unified_changes
